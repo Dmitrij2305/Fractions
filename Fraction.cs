2a7177ba025aa9d0d752113fc2bf08f863658d32
Dmitrij2305 <romanov.yar357@mail.ru>
2017-05-08 19:21:04 +0300

    Написан начальный код без тестов
    Написан конструктор; свойства, возвращающие числитель и знаменатель;
    методы перевода дроби в строку и строки в дробь.

--- Fraction.cs
+++ Fraction.cs
@@ -6,7 +6,38 @@ using System.Threading.Tasks;
 
 namespace Fractions
 {
-    public class Fraction
+    class Fraction
     {
+        private int numerator;
+        private int denominator;
+
+        public int Numerator
+        {
+            get { return numerator; }
+        }
+
+        public int Denominator
+        {
+            get { return denominator; }
+        }
+
+        public Fraction(int numerator, int denominator)
+        {
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0}/{1}", numerator, denominator);
+        }
+
+        public Fraction ParseFraction(string fraction)
+        {
+            numerator = int.Parse(fraction.Split(new char[] { '/', ' ' })[0]);
+            denominator = int.Parse(fraction.Split(new char[] { '/', ' ' })[1]);
+
+            return new Fraction(numerator, denominator);
+        }
     }
 }
